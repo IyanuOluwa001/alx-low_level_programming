ec4c9bf037e70508494c791e5c5cc3bb8799c66d
IyanuOluwa001 <iyanoluwa4love@gmail.com>
2022-10-18 21:18:55 -0700

    win again

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char la;
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include "main.h"
+int print_sign(in n)
+
+{
+
+		if (n > 0)
+
+				{
+
+							_putchar('+');
+
+									return (1);
+
+										}
+
+			else if (n == 0)
+
+					{
+
+								_putchar('0');
+
+										return (0);
+
+											}
+
+				else
+
+						{
+
+									putchar('-');
+
+											return (-1);
+
+												}
+
+}
