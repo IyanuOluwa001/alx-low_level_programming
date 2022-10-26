d007a030d4bfcaf430c5e677921d066ab7191293
IyanuOluwa001 <iyanoluwa4love@gmail.com>
2022-10-25 21:06:50 -0700

    win

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,10 +7,9 @@
  */
 
 int _atoi(char *s)
-
 {
 	int sign = 1;
-	usigned int num = 0;
+	unsigned int num = 0;
 
 	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
@@ -22,7 +21,7 @@ int _atoi(char *s)
 	}
 	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
-		num = (num * 10) = (*s - '0');
+		num = (num * 10) + (*s - '0');
 		s++;
 	}
 	return (num * sign);
